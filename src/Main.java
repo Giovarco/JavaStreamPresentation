11f19d337fa668742fb1240a11a7afcf87d846e7
Unknown <kirby94@hotmail.it>
2019-02-02 15:28:31 +0100

    Example 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+        myList
+            .stream()
+            .filter(s -> s.startsWith("c"))
+            .map(String::toUpperCase)
+            .sorted()
+            .forEach(System.out::println);
+
+        // C1
+        // C2
     }
+    
 }
